595adc4b8aba7fa0da4ced78f0e61d4801c312c2
sakaputra1922 <sakap474@gmail.com>
2024-12-04 13:32:27 +0700

    Add files via upload

---
+++ vid-php2/index.php
@@ -0,0 +1,21 @@
+<?php
+
+$tulisan = "Makan Dan Minuman Hanya Di Indonesia";  
+
+echo $tulisan;
+
+echo "<br>";
+
+
+
+$angka = '2024';
+
+echo 'Tahun ' .$angka. '<br>';
+
+var_dump($tulisan);
+echo '<br>';
+
+
+var_dump((int)$angka+3-2);
+
+?>
